13c1ec40044f3fea5c25c8247a4abcea2add9930
Segun Famisa <segunfamisa@gmail.com>
2021-03-13 12:45:58 +0100

    Implement design for login screen

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/LoginScreen.kt
@@ -0,0 +1,156 @@
+package com.example.androiddevchallenge.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+
+@Composable
+fun LoginScreen(
+    onLogInClick: (String, String) -> Unit,
+    onLogInSuccessful: () -> Unit,
+    onSignUpClick: () -> Unit
+) {
+    val loginBg = if (MaterialTheme.colors.isLight) {
+        painterResource(id = R.drawable.light_login_bg)
+    } else {
+        painterResource(id = R.drawable.dark_login_bg)
+    }
+    val focusManager = LocalFocusManager.current
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        Image(painter = loginBg, modifier = Modifier.fillMaxSize(), contentDescription = "")
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .wrapContentHeight(align = Alignment.CenterVertically)
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.CenterHorizontally),
+                text = stringResource(id = R.string.log_in), style = MaterialTheme.typography.h1
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            InputField(
+                hint = stringResource(id = R.string.hint_email),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                onValueChange = { email = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    }
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            InputField(
+                hint = stringResource(id = R.string.hint_pwd),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .focusable(enabled = true),
+                onValueChange = { password = it },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
+                )
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                modifier = Modifier
+                    .height(72.dp)
+                    .fillMaxWidth(),
+                shape = MaterialTheme.shapes.medium,
+                onClick = { onLogInClick(email, password) }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.log_in),
+                    style = MaterialTheme.typography.button,
+                )
+            }
+            Text(
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .paddingFromBaseline(32.dp)
+                    .fillMaxWidth()
+                    .wrapContentWidth(align = Alignment.CenterHorizontally),
+                text = buildAnnotatedString {
+                    append("Don't have an account? ")
+                    withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+                        append("Sign up")
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun InputField(
+    hint: String,
+    modifier: Modifier = Modifier,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    onValueChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
+) {
+    var value by remember { mutableStateOf("") }
+    TextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = {
+            value = it
+            onValueChange(it)
+        },
+        label = {
+            Text(
+                text = hint,
+                style = MaterialTheme.typography.body1,
+                color = contentColorFor(backgroundColor = MaterialTheme.colors.surface)
+            )
+        },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        visualTransformation = visualTransformation,
+        textStyle = MaterialTheme.typography.body1,
+        singleLine = true,
+        maxLines = 1
+    )
+}
