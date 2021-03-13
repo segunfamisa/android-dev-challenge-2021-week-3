5559451acb814bc5a8bbd0f4b1aa94890a4be800
Segun Famisa <segunfamisa@gmail.com>
2021-03-13 15:33:29 +0100

    Move "spacedForEach" into common

--- app/src/main/java/com/example/androiddevchallenge/ui/home/Common.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/Common.kt
@@ -1,17 +1,25 @@
 package com.example.androiddevchallenge.ui.home
 
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
-fun SectionHeader(text: String) {
+fun <T> List<T>.spacedForEach(spacer: @Composable () -> Unit, action: @Composable (T) -> Unit) {
+    this.forEachIndexed { index, item ->
+        if (index != size - 1) {
+            spacer()
+        }
+        action(item)
+    }
+}
+
+@Composable
+fun SectionHeader(text: String, modifier: Modifier = Modifier) {
     Text(
         text = text,
         style = MaterialTheme.typography.h2,
-        modifier = Modifier.paddingFromBaseline(top = 40.dp)
+        modifier = modifier
     )
 }
