8d6c673c4405a7d4539b29cf20c0bcc3ca90e65e
Segun Famisa <segunfamisa@gmail.com>
2021-03-13 13:01:23 +0100

    Add navigation controller and routes

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,8 +18,14 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.LoginScreen
+import com.example.androiddevchallenge.ui.Routes
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.androiddevchallenge.ui.welcome.WelcomeScreen
 
@@ -37,21 +43,28 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    WelcomeScreen(onSignUpClick = { }, onLogInClick = {})
-}
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.Welcome) {
+        composable(Routes.Welcome) {
+            WelcomeScreen(onSignUpClick = {
+                navController.navigate(Routes.SignUp)
+            }, onLogInClick = {
+                navController.navigate(Routes.LogIn)
+            })
+        }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
+        composable(Routes.SignUp) {
+            Text("Sign up screen")
+        }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+        composable(Routes.LogIn) {
+            LoginScreen(
+                onLogInClick = { _, _ -> },
+                onLogInSuccessful = {},
+                onSignUpClick = {
+                    navController.navigate(Routes.SignUp)
+                }
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/ui/Routes.kt
@@ -0,0 +1,7 @@
+package com.example.androiddevchallenge.ui
+
+object Routes {
+    const val Welcome = "/welcome"
+    const val SignUp = "/signup"
+    const val LogIn = "/login"
+}
