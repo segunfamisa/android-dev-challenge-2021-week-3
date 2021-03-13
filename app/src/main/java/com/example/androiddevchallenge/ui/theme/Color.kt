b90994d2e65a31b00ceb3892a169c4c2a0cb8a70
Segun Famisa <segunfamisa@gmail.com>
2021-03-13 11:45:54 +0100

    Add FF prefix to colors

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Color.kt
@@ -18,14 +18,14 @@ package com.example.androiddevchallenge.ui.theme
 import androidx.compose.ui.graphics.Color
 
 
-val grey900 = Color(0x333333)
+val grey900 = Color(0xFF333333)
 val grey800 = Color(0xCC333333)
 
-val taupe100 = Color(0xF0EAE2)
-val taupe800 = Color(0x655454)
+val taupe100 = Color(0xFFF0EAE2)
+val taupe800 = Color(0xFF655454)
 
-val rust300 = Color(0xE1AFAF)
-val rust600 = Color(0x886363)
+val rust300 = Color(0xFFE1AFAF)
+val rust600 = Color(0xFF886363)
 
 val white150 = Color(0x24FFFFFF)
 val white800 = Color(0xCCFFFFFF)
